0e74b6da7f9bd382d8ac7c6005027ff09079af55
eggc <no.eggchicken@gmail.com>
2022-04-10 15:32:57 +0900

    🆕 brew bundle を実行するようにした

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,15 @@
 #!/bin/zsh
 set -eu
 
+NOCOLOR='\033[0m'
+GREEN='\033[0;32m'
+
+function green_echo() {
+    echo "${GREEN}$1${NOCOLOR}"
+}
+
 function install_brew() {
-    echo "Install brew:"
+    green_echo "Install brew:"
     echo "  You should confirm the following commmand from https://brew.sh/index_ja"
     echo "  \$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     read -k "YES_OR_NO?Ok? (y/n): "
@@ -12,4 +19,21 @@ function install_brew() {
     fi
 }
 
+function brew_bundle() {
+    green_echo "Install packages by brew bundle:"
+    mkdir -p ~/tmp/setup/
+    cd ~/tmp/setup
+    curl -fsSL "https://raw.githubusercontent.com/eggc/setup/main/lib/Brewfile" > Brewfile
+
+    echo "  You will install or upgrade following packages"
+    cat Brewfile
+    read -k "YES_OR_NO?Ok? (y/n): "
+
+    if [[ $YES_OR_NO == y ]]; then
+        brew bundle -v
+    fi
+}
+
 type brew || install_brew
+
+brew_bundle
