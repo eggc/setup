6ea2540a297ae79130cd6aca3b82866c819aa932
eggc <no.eggchicken@gmail.com>
2022-04-10 16:00:01 +0900

    🏥 homebrew インストール後に環境変数の設定もやる

--- setup.zsh
+++ setup.zsh
@@ -20,6 +20,9 @@ function install_brew() {
         curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
         chmod +x ./install.sh
         ./install.sh
+
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 }
 
